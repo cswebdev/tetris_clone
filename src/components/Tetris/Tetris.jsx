aedf24313f2477205794e29b7fb40bb4bd75470e
cswebdev <csnider91@live.com>
2024-02-17 16:47:55 -0500

    created the tetrominoes and rendered them in preview grid

--- src/components/Tetris/Tetris.jsx
+++ src/components/Tetris/Tetris.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import "./Tetris.css";
 import Board from "../Board/Board";
-import { useBoard } from "../../hooks/useBoard";
+import Previews from "../Previews/Previews";
 import GameStats from "../GameStats/GameStats";
 
-const Tetris = ({ rows, columns, setGameOver, gameStats }) => {
+import { useBoard } from "../../hooks/useBoard";
+import { useGameStats } from "../../hooks/useGameStats";
+import { usePlayer } from "../../hooks/usePlayer";
+
+const Tetris = ({ rows, columns, setGameOver }) => {
+   const [gameStats, addLinesCleared] = useGameStats();
    const [board, setBoard] = useBoard({ rows, columns });
+   const [player, setPlayer, resetPlayer] = usePlayer();
 
    return (
       <div className="Tetris">
          <Board board={board} />;
          <GameStats gameStats={gameStats} />
+         <Previews tetrominoes={player.tetrominoes} />
       </div>
    );
 };
--- src/utils/Board.js
+++ src/utils/Board.js
@@ -4,7 +4,7 @@ export const buildBoard = ({ rows, columns }) => {
    const builtRows = Array.from({ length: rows }, () =>
       Array.from({ length: columns }, () => ({ ...defaultCell }))
    );
-
+    
    return {
       rows: builtRows,
       size: { rows, columns },

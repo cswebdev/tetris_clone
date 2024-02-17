0f66ebc1adc638782f25700a121af2c1481c71fd
cswebdev <csnider91@live.com>
2024-02-17 11:40:59 -0500

    created basic hook for game status

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Game.css";
 import Menu from "../Menu/Menu";
+import { useGameOver } from "../../hooks/useGameOver";
 
 const Game = ({ rows, columns }) => {
+   const [gameOver, setGameOver, resetGameOver] = useGameOver();
    const start = () => {
-      console.log("start");
+      console.log(`start gameOver ${gameOver}`);
    };
 
    return (

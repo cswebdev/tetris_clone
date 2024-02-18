1a1c3e5e99363d68233a189dd9ead9400b0d65ec
cswebdev <csnider91@live.com>
2024-02-17 20:32:16 -0500

    creating hooks to update the game board

--- src/hooks/useBoard.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import { buildBoard } from "../utils/Board";
-
-export const useBoard = ({ rows, columns }) => {
-   const [board] = useState(buildBoard({ rows, columns }));
-
-   return [board];
-};
--- src/utils/Board.js
+++ src/utils/Board.js
@@ -4,9 +4,19 @@ export const buildBoard = ({ rows, columns }) => {
    const builtRows = Array.from({ length: rows }, () =>
       Array.from({ length: columns }, () => ({ ...defaultCell }))
    );
-    
+
    return {
       rows: builtRows,
       size: { rows, columns },
    };
+};
+
+export const nextBoard = ({ board, player, resetPlayer, addLinesCleared }) => {
+   const { tetromino, position } = player;
+
+   //copy & clear spaces used by pieces
+   // that hadn't collided & occupied spaces permanently
+   let rows = board.rows.map((row) =>
+      row.map((cell) => (cell.occupied ? cell : { ...defaultCell }))
+   );
 };
